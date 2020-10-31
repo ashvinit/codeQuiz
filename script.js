c3b6191aa094b084598869068ba0ec5beb87fc0c
Ashvini Thomas <ashvinithomas@gmail.com>
2020-10-31 12:41:16 -0400

    added variables to js

---
+++ script.js
@@ -0,0 +1,5 @@
+//variables
+var highScoresEl = document.querySelector(".highScores");
+var timerEl = document.querySelector(".timer");
+var quizBoxEl = document.querySelector(".quizBox");
+var startBtn = document.querySelector(".startQuiz");
