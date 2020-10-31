2dcb82b2eec032ee796dd20b913848f4a290e1f3
Ashvini Thomas <ashvinithomas@gmail.com>
2020-10-31 12:53:15 -0400

    added function for when start button is clicked

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ var highScoresEl = document.querySelector(".highScores");
 var timerEl = document.querySelector(".timer");
 var quizBoxEl = document.querySelector(".quizBox");
 var startBtn = document.querySelector(".startQuiz");
+
+
+//When start button is clicked, start timer & presebnt questions
+startBtn.addEventListener("click", function() {
+    startTimer();
+    questions();
+});
