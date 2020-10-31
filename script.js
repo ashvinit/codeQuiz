1a96965fc7c94e7b37d2e4355503144e1f673bda
Ashvini Thomas <ashvinithomas@gmail.com>
2020-10-31 13:23:43 -0400

    added timer function

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ var timerEl = document.querySelector(".timer");
 var quizBoxEl = document.querySelector(".quizBox");
 var startBtn = document.querySelector(".startQuiz");
 
+var secondsLeft = 60;
+
+//start timer
+function startTimer() {
+    timerEl.style.display = "block";
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = "Time Left: " + secondsLeft + " seconds";
+    
+        if(secondsLeft === 0) {
+          clearInterval(timerInterval);
+          addScore();
+        }
+    
+      }, 1000);
+}
+
 
 //When start button is clicked, start timer & presebnt questions
 startBtn.addEventListener("click", function() {
